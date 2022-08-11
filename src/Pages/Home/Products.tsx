2d79745bc824f4d66e7bfa5bf1aa55560db8439e
GeorgeKrs <43423876+GeorgeKrs@users.noreply.github.com>
2022-08-11 14:06:20 +0300

    products name

--- src/Pages/Home/Products.tsx
+++ src/Pages/Home/Products.tsx
@@ -13,40 +13,40 @@ const Products = ({ selectedCategory }: Props) => {
       id: 1,
       imageUrl:
         "https://images.unsplash.com/photo-1504674900247-0877df9cc836?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8MXx8fGVufDB8fHx8&w=1000&q=80",
-      name: "Plate Name",
-      price: 15.99,
+      name: "Beef & Pork",
+      price: 27.99,
       description:
-        "Some quick example text to build on the card title and make up the bulk of the card's content.",
+        "Some description text to provide information such as name and price about the item on the card.",
       category: "Main Dishes",
     },
     {
       id: 2,
       imageUrl:
         "https://restaurantden.com/wp-content/uploads/2017/09/free-stock-food-photography-websites.jpg",
-      name: "Plate Name",
-      price: 12.49,
+      name: "Fish Plate",
+      price: 17.99,
       description:
-        "Some quick example text to build on the card title and make up the bulk of the card's content.",
+        "Some description text to provide information such as name and price about the item on the card.",
       category: "Main Dishes",
     },
     {
       id: 3,
       imageUrl:
         "https://static6.depositphotos.com/1066961/553/i/450/depositphotos_5534545-stock-photo-salmon-with-avocado-salsa.jpg",
-      name: "Plate Name",
-      price: 7.0,
+      name: "Chicken & Salad",
+      price: 12.99,
       description:
-        "Some quick example text to build on the card title and make up the bulk of the card's content.",
+        "Some description text to provide information such as name and price about the item on the card.",
       category: "Main Dishes",
     },
     {
       id: 4,
       imageUrl:
         "https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg?quality=90&resize=768,574",
-      name: "Plate Name",
-      price: 11.99,
+      name: "Tomato Pasta",
+      price: 14.99,
       description:
-        "Some quick example text to build on the card title and make up the bulk of the card's content.",
+        "Some description text to provide information such as name and price about the item on the card.",
       category: "Main Dishes",
     },
 
@@ -54,60 +54,60 @@ const Products = ({ selectedCategory }: Props) => {
       id: 5,
       imageUrl:
         "https://kalynskitchen.com/wp-content/uploads/2014/12/2-650-sweet-potato-appetizer-bites-9-kalynskitchen.jpg",
-      name: "Plate Name",
-      price: 3.5,
+      name: "Sweet Potato Snacks",
+      price: 6.0,
       description:
-        "Some quick example text to build on the card title and make up the bulk of the card's content.",
+        "Some description text to provide information such as name and price about the item on the card.",
       category: "Appetizers",
     },
     {
       id: 6,
       imageUrl:
         "https://media.istockphoto.com/photos/beautifully-decorated-catering-banquet-with-different-food-snacks-and-picture-id1165401900?k=20&m=1165401900&s=612x612&w=0&h=Ub6xFjMbfyQRKCUT82pd_fN9Iz02Rk-FQzkIQIaRZ0I=",
-      name: "Plate Name",
-      price: 3.5,
+      name: "Mixed Finger Food",
+      price: 8.0,
       description:
-        "Some quick example text to build on the card title and make up the bulk of the card's content.",
+        "Some description text to provide information such as name and price about the item on the card.",
       category: "Appetizers",
     },
     {
       id: 7,
       imageUrl:
         "https://www.acouplecooks.com/wp-content/uploads/2022/01/Smoked-Salmon-Appetizer-006.jpg",
-      name: "Plate Name",
-      price: 3.5,
+      name: "Salmon Finger Food",
+      price: 7.99,
       description:
-        "Some quick example text to build on the card title and make up the bulk of the card's content.",
+        "Some description text to provide information such as name and price about the item on the card.",
       category: "Appetizers",
     },
     {
       id: 8,
       imageUrl:
         "https://www.fix-beer.gr/wp-content/uploads/2019/02/Fix-Hellas-Outside-Image.png",
-      name: "Plate Name",
+      name: "Fix Beer",
       price: 3.5,
       description:
-        "Some quick example text to build on the card title and make up the bulk of the card's content.",
+        "Some description text to provide information such as name and price about the item on the card.",
       category: "Drinks",
     },
     {
       id: 9,
       imageUrl:
         "https://amperiadis.gr/wp-content/uploads/2020/11/Bernard-Beers-2.3.jpg",
-      name: "Plate Name",
-      price: 3.5,
+      name: "Bernard Beer",
+      price: 6.0,
       description:
-        "Some quick example text to build on the card title and make up the bulk of the card's content.",
+        "Some description text to provide information such as name and price about the item on the card.",
       category: "Drinks",
     },
     {
       id: 10,
       imageUrl:
         "https://www.edelweissbeer.com/media/0gthn2ir/edelweiss-bottle-homepage-blue-background-new-bottle.png",
-      name: "Plate Name",
-      price: 3.5,
+      name: "Edelweiss",
+      price: 5.5,
       description:
-        "Some quick example text to build on the card title and make up the bulk of the card's content.",
+        "Some description text to provide information such as name and price about the item on the card.",
       category: "Drinks",
     },
   ];
